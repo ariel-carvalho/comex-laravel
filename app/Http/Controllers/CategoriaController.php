d452331679454d414ac02588240c3e028ee0d987
Ariel Carvalho <ariell_carvalho@hotmail.com>
2023-12-03 12:40:27 -0300

    Categorias no SQLite

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Categoria;
 
 class CategoriaController extends Controller
 {
@@ -11,7 +12,7 @@ class CategoriaController extends Controller
      */
     public function index()
     {
-        $categorias = 
+        /*$categorias = 
         [
             [
                 "nome" => "CELULARES"
@@ -37,8 +38,9 @@ class CategoriaController extends Controller
             [
                 "nome" => "LUXO"
             ]
-        ];
+        ];*/
         
+        $categorias = Categoria::all();
         return view("categorias.index", ["categorias" => $categorias]);
     }
 
